58468fec0513cd32f0a9b235288240ad463c0f43
David Cuellar <david.cuellar@javeriana.edu.co>
2021-12-23 18:46:59 -0500

    arrays desafío

--- js/script.js
+++ js/script.js
@@ -8,12 +8,13 @@ let flagMore = true;
 let flagGood = true;
 let flagAll = false;
 let finalPrice = parseFloat(0);
-
+let prodLen;
 
 //Class product paint
 class paint {
-  constructor(name, price) {
-    this.name  = name
+  constructor(author,name, price) {
+    this.author = author;
+    this.name  = name;
     this.price  = parseFloat(price);
     this.sold = false;
   }
@@ -27,9 +28,17 @@ class paint {
 }
 
 // New paints
-const paint1 = new paint("Salvator Mundi", "450300000");
-const paint2 = new paint("Interchange", "300000000");
-const paint3 = new paint("Los Jugadores de cartas", "250000000");
+const paint1 = new paint("Leonardo da Vinci","Salvator Mundi", "450300000");
+const paint2 = new paint("Willem de Kooning","Interchange", "300000000");
+const paint3 = new paint("Pierre-Auguste Renoir","Bal du moulin de la Galette", "78100000");
+
+//Array of paints
+const products = [paint1, paint2, paint3]
+
+//Use of Push in array
+products.push(new paint("Vincent Van Gogh","Retrato del Dr. Gachet", "82500000"))
+products.push(new paint("Andy Warhol","Orange Marilyn", "241000000"))
+
 
 // Calculate the total of the purchase in Millions
 const totalM = () => total / 1000000;
@@ -51,7 +60,7 @@ function other(){
 
 // Cicle of the script
 while (flagMore == true){
-  entrada = prompt('Escribe el número de la pintura que deseas comprar \n Recuerda que no puedes comprar la pintura dos veces \n 1. Salvator Mundi	$450.3 millones \n 2. Interchange	$300 millones \n 3. Los Jugadores de cartas	$250 millones \n \n Recuerda el valor que al valor de la pintura se le sumará el valor del IVA del 19%');
+  entrada = prompt('Escribe el número de la pintura que deseas comprar \n Recuerda que no puedes comprar la pintura dos veces \n 1. Salvator Mundi	$450.3 millones \n 2. Interchange	$300 millones \n 3. Bal du moulin de la Galette \n Pronto encontrarás nuevas pinturas\n \n Recuerda el valor que al valor de la pintura se le sumará el valor del IVA del 19%');
   numero = parseInt(entrada);
 
   if (numero == 1 || numero == 2 || numero == 3){
@@ -106,3 +115,62 @@ if (flagGood == true){
   alert("Usted ingresó un dato invalido, por favor refresque la página y vuelva a intentarlo");
 }
 
+//Print in console all the products name in default order
+prodLen = products.length;
+
+console.log("Todas las pinturas en orden por defecto son: ");
+for (let i = 0; i < prodLen ; i++){
+  console.log("Obra: " + products[i].name + "Valor neto: " + products[i].price);
+}
+
+
+// sort by value high to low my function
+const prodSort = [];
+let high=1000000000000000;
+let tempNum = 0;
+let sortNum;
+const prodSortName = products;
+
+for (let j = 0; j < prodLen ; j++){
+  for (let y = 0; y < prodLen ; y++){
+    if (products[y].price > tempNum && products[y].price < high){
+      tempNum =  products[y].price;
+      sortNum = y;
+    }
+  }
+  prodSort[j] = products[sortNum];
+  high = tempNum;
+  tempNum=0;
+}
+
+//Print in console all the products by value order with my function
+
+console.log(" ");
+console.log("Mi función");
+console.log("Todas las pinturas en orden de mayor a menor son: ");
+for (let i = 0; i < prodLen ; i++){
+  console.log("Obra: " + prodSort[i].name + "Valor neto: " + prodSort[i].price);
+}
+
+
+
+//using sort function
+prodSortName.sort(function (a, b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+});
+
+//Print in console all the products by name with Sort
+
+console.log(" ");
+console.log("Función Sort");
+console.log("Todas las pinturas en orden alfabético son: ");
+for (let i = 0; i < prodLen ; i++){
+  console.log("Obra: " + prodSortName[i].name + "Valor neto: " + prodSortName[i].price);
+}
