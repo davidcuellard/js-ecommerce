8919cfb21231a62a3d50371c50204b5790a8a4fb
David Cuellar <david.cuellar@javeriana.edu.co>
2022-01-31 21:42:18 -0500

    animaciones concatenadas fixed

--- js/components/animations.js
+++ js/components/animations.js
@@ -10,12 +10,7 @@ $(document).ready(function() {
 $("button").hover(function(){
   $( this ).css("background-color", "green")
            .css("color", "white")
-           .fadeOut( 0 )
+           .fadeOut( 10 )
            .fadeIn( 1000 )
-  },function(){
-    $( this ).css("background-color", "rgb(255, 253, 110)")
-            .css("color", "rgb(0, 0, 0)")
-            .fadeOut( 0 )
-            .fadeIn( 1000 )
   }
 );
